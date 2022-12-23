9c13160f29883588990d062f3005e22b6fb4b9cf
AryanMore <aryanmore498@gmail.com>
2022-12-23 17:54:55 +0530

    allset

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent = "Welcome, I am Aryan from Navsari, Gujarat, India.I am currently developing my skills to get good job opportunities and also i love to learn new things from people.";
+const aboutContent = "My Name Is Aryan More. Currently I Am Pursuing My Bachelor In Computer Application (B.CA) Honors from College Of Professional Studies And Applied Sciences Chikli Gujarat Affiliated with Veer Narmad South Gujarat University. I Enjoy Learning About New Technologies And Have Passion For Web Developement.";
+const contactContent = "E-mail: aryanmore498@gmail.com || Phone Number: 9925034124 || LinkedIn: https://www.linkedin.com/in/aryan-more-417497216/ || Twitter: https://twitter.com/aRYNMore2110";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
